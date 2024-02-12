58c3dd4f55dac421a0e5c1a7f6696b134c5e979c
coderxdesigner <rachel.heneault@gmail.com>
2024-02-12 11:02:09 -0500

    Tutorial notes

---
+++ Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        final String message = "Hello World Variable";
+        final int youth = 49;
+        final int future = 5;
+        System.out.println("Hello " + message);
+        System.out.println(youth + future);
+        // comments are easy
+    }
+}
+
+/*
+ * JAVA VARIABLES
+ * data types....
+ * String - stores text
+ * int - stores whole number integers no decimals
+ * float - stores floating point numbers - yes decimals
+ * char - stores single character such a or B
+ * boolean - true or false
+ * 
+ * PRIMITIVE DATA TYPES
+ * specifies the size and type of variable values, no additional methods
+ * eight types are:
+ * byte, short, int, long, float, double, boolean, char
+ *  NAMING CONVENTIONS
+ * Can contain:letters, digits, underscores and dollar signs
+ * must being with a LETTER
+ * should start with a lowercase letter and no whitespace please
+ * names can beging with $ or _
+ * names are case sensitive
+ * don't use reserved keywords - duh
+ */
